c3ec219e26e15741179c252bda31ee6e821d458c
Salomelette <salomeattiach@hotmail.fr>
2018-11-23 00:05:40 +0100

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -10,66 +10,72 @@ Created on Thu Nov 22 18:42:57 2018
 #import numpy as np
 
 #recuperation du nombre de villes 
-f = open("/users/Etu3/3520413/mogpl/projet/Data/villes92.txt")
-
-n=0
-for data in f:
-    #print(data)
-    n+=1
-    
-f.close()
-
-#recuperation des distances
-f = open("/users/Etu3/3520413/mogpl/projet/Data/distances92.txt")
-
-dij=[]
-for data in f:
-    if type(data)=<class 'str'>:
-        for j in range(n):
-            
-    
-
-
-#m1 = model("question1")
+#f = open("/home/salom/mogpl/projet/Data/villes92.txt")
 #
-#X = []
-#J = [2,5,14]
-#k = 3
-#a = 0.1
+#n=0
+#for data in f:
+#    #print(data)
+#    n+=1
+#    
+#f.close()
 #
-#for i in range(k):
-#    for j in range(n):
+##recuperation des distances
+#f = open("/home/salom/mogpl/projet/Data/distances92.txt")
+#
+#dij = []
+#file = f.read().splitlines()
+#for i in range(len(file)):
+#    if type(file[i]) == str:
 #        aux = []
-#        aux.append(m1.addVar(vtype=GRB.BINARY))
-#    X.append(aux)
-#    
-#m1.update()
-#
-#fo = LinExpr()
-#fo = 0
-#
-#for i in range(n):
-#    for j in range(k):
-#        fo+=d[i][J[j]]*X[i][j]
-#        
-#m1.setObjective(obj,GRB.MAXIMIZE)
-#
-#for i in range(n):
-#    s = 0
-#    for j in range(k):
-#        s+=X[i][j] #correspond a la somme des xij sur j 
-#        
-#    m1.addConstr(s=1) #contrainte 1: une ville appartient à un unique secteur
-#    
-#for j in range(k):
-#    s1=0
-#    s2=0
-#    for i in range(n):
-#        s1+=X[i][j]*v[i] #somme des populations totales des villes composant un secteur
-#        s2+=v[i] #somme des populations totales des villes
-#    
-#    m1.addConstr((s1/s2)<=((1+a)/k)) #contrainte 2: populations totales des villes composant un secteur ne doit pas dépasser gamma (voir poly)
-
+#        for j in range(1,n):
+#            file[i+j] = float(file[i+j])
+#            print(file[i+j])
+#            aux.append(file[i+j]) 
+#        dij.append(aux)
         
     
+
+
+m1 = model("question1")
+
+X = []
+J = [2,5,14]
+k = 3
+a = 0.1
+
+for i in range(k):
+    for j in range(n):
+        aux = []
+        aux.append(m1.addVar(vtype=GRB.BINARY))
+    X.append(aux)
+    
+m1.update()
+
+fo = LinExpr()
+fo = 0
+
+for i in range(n):
+    for j in range(k):
+        fo+=d[i][J[j]]*X[i][j]
+        
+m1.setObjective(obj,GRB.MAXIMIZE)
+
+for i in range(n):
+    s = 0
+    for j in range(k):
+        s+=X[i][j] #correspond a la somme des xij sur j 
+        
+    m1.addConstr(s=1) #contrainte 1: une ville appartient à un unique secteur
+    
+for j in range(k):
+    s1=0
+    s2=0
+    for i in range(n):
+        s1+=X[i][j]*v[i] #somme des populations totales des villes composant un secteur
+        s2+=v[i] #somme des populations totales des villes
+    
+    m1.addConstr((s1/s2)<=((1+a)/k)) #contrainte 2: populations totales des villes composant un secteur ne doit pas dépasser gamma (voir poly)
+
+
+m1.optimize()
         
